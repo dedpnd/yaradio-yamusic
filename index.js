f97293f2a0ad302a4c56809f62361ed639742195
dedpnd <pr.deadpanda@gmail.com>
2018-06-25 22:02:19 +0300

    fix img + min size

--- index.js
+++ index.js
@@ -1,89 +1,89 @@
 const path = require('path');
 const fs = require('mz/fs');
-const { app, BrowserWindow  } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const store = require('./modules/store/store');
 const ctxMenu = require('./modules/menu/ctxMenu');
 
-if(process.env.node_env == 'dev') require('electron-debug')({enabled: true, showDevTools: 'undocked'});
+if (process.env.node_env == 'dev') require('electron-debug')({ enabled: true, showDevTools: 'undocked' });
 
 let win
 
 app.makeSingleInstance(() => {
-	if (win) {
-		if (win.isMinimized()) {
-			win.restore();
-		}
-		win.focus();
-	}
+    if (win) {
+        if (win.isMinimized()) {
+            win.restore();
+        }
+        win.focus();
+    }
 });
 
-function createWindow(){
-  const lastWindowState = store.get('lastWindowState');
-  
-  const win = new BrowserWindow({
-      title: 'YaRadio',
-      show: false,
-      x: lastWindowState.x,
-      y: lastWindowState.y,
-      height: lastWindowState.height || 750,
-      width: lastWindowState.width || 850,        
-      icon: path.join(__dirname,'media/icon','yaradio.png'),      
-      titleBarStyle: 'hiddenInset',
-      minHeight: 750,
-      minWidth: 850,
-      autoHideMenuBar: true,
-      backgroundColor: '#fff',
-      webPreferences: {   
-        preload: path.join(__dirname,'modules/js', 'browser.js'),     
-        nodeIntegration: false,
-        plugins: true
-      }
-  })  
-  // https://music.yandex.ru/
-  win.loadURL('https://radio.yandex.ru/');
+function createWindow() {
+    const lastWindowState = store.get('lastWindowState');
 
-  win.on('close', e => {
-    if (!store.get('quit?')) {
-      e.preventDefault();
-    }
+    const win = new BrowserWindow({
+            title: 'YaRadio',
+            show: false,
+            x: lastWindowState.x,
+            y: lastWindowState.y,
+            height: lastWindowState.height || 700,
+            width: lastWindowState.width || 800,
+            icon: path.join(__dirname, 'media/icon', 'yaradio.png'),
+            titleBarStyle: 'hiddenInset',
+            minHeight: 700,
+            minWidth: 800,
+            autoHideMenuBar: true,
+            backgroundColor: '#fff',
+            webPreferences: {
+                preload: path.join(__dirname, 'modules/js', 'browser.js'),
+                nodeIntegration: false,
+                plugins: true
+            }
+        })
+        // https://music.yandex.ru/
+    win.loadURL('https://radio.yandex.ru/');
 
-    switch(process.platform) {
-      case 'win32':
-        win.hide();
-        break;
-      case 'linux':
-        win.minimize();
-        break;
-      case 'darwin':
-        app.hide();
-        break;
-      default:
-    }		
-	});
+    win.on('close', e => {
+        if (!store.get('quit?')) {
+            e.preventDefault();
+        }
 
-  win.on('page-title-updated', e => {
-		e.preventDefault();
-	});
+        switch (process.platform) {
+            case 'win32':
+                win.hide();
+                break;
+            case 'linux':
+                win.minimize();
+                break;
+            case 'darwin':
+                app.hide();
+                break;
+            default:
+        }
+    });
 
-  return win
+    win.on('page-title-updated', e => {
+        e.preventDefault();
+    });
+
+    return win
 }
 
-app.on("ready", ()=>{ 
-  win = createWindow()
-  ctxMenu.create(win, app);
-  win.setMenu(null);
+app.on("ready", () => {
+    win = createWindow()
+    ctxMenu.create(win, app);
+    win.setMenu(null);
 
-  let page = win.webContents;
-  page.on('dom-ready', ()=>{ 
-    page.insertCSS(fs.readFileSync(path.join(__dirname, '/modules/css', 'css.css'), 'utf8'));
-    win.show();
-  })
+    let page = win.webContents;
+    page.on('dom-ready', () => {
+        page.insertCSS(fs.readFileSync(path.join(__dirname, '/modules/css', 'css.css'), 'utf8'));
+        win.show();
+    })
 })
 
 app.on('before-quit', () => {
-	store.set('quit?', true);	
+    store.set('quit?', true);
 
-	if (!win.isFullScreen()) {
-		store.set('lastWindowState', win.getBounds());
-	}
+    if (!win.isFullScreen()) {
+        store.set('lastWindowState', win.getBounds());
+    }
 });
--- modules/js/browser.js
+++ modules/js/browser.js
@@ -1,25 +1,25 @@
 const { ipcRenderer, webFrame, shell } = require('electron');
 
 const el = {
-	prefButton: '.page-root .settings',
-	prefDialog: '.settings-stream.popup',
-	mute: '.page-root .volume',
-	play: '.page-station .player-controls__play',
-	next: '.page-station .slider__item_next',
-	like: '.page-station .button.like_action_like',
-	dislike: '.page-station .button.like_action_dislike',
-	activeStation: '.page-index .station_playing'
+    prefButton: '.page-root .settings',
+    prefDialog: '.settings-stream.popup',
+    mute: '.page-root .volume',
+    play: '.page-station .player-controls__play',
+    next: '.page-station .slider__item_next',
+    like: '.page-station .button.like_action_like',
+    dislike: '.page-station .button.like_action_dislike',
+    activeStation: '.page-index .station_playing'
 };
 
 function exec(command) {
-	webFrame.executeJavaScript(`if (!window.a) a = new Mu.Adapter(); ${command};`);
+    webFrame.executeJavaScript(`if (!window.a) a = new Mu.Adapter(); ${command};`);
 }
 
 function click(s) {
-	const e = document.querySelector(s);
-	if (e) {
-		e.click();
-	}
+    const e = document.querySelector(s);
+    if (e) {
+        e.click();
+    }
 }
 
 ipcRenderer.on('play', () => exec('a.togglePause()'));
@@ -29,35 +29,35 @@ ipcRenderer.on('dislike', () => click(el.dislike));
 ipcRenderer.on('mute', () => exec('a.mute()'));
 ipcRenderer.on('HQ', () => exec('a.toggleHQ()'));
 
-document.onreadystatechange = ()=>{ 
+document.onreadystatechange = () => {
     //Remove href to yandex
     $('div.footer__left').find('.link').removeAttr('href');
-    $('div.nav__level').find('.footer__static-text').find('.link').removeAttr('href');  
+    $('div.nav__level').find('.footer__static-text').find('.link').removeAttr('href');
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{   
-  if(/radio/.test(location.origin)){
-    initRadio();
-  }
+document.addEventListener("DOMContentLoaded", () => {
+    if (/radio/.test(location.origin)) {
+        initRadio();
+    }
 });
 
-function initRadio(){
-  //Add href to GitHub
-  let cloneFooterElement = null;
-  $('div.footer__right').find('div').each((i,e)=>{     
-    $(e).css('display', 'none');
-  })
+function initRadio() {
+    //Add href to GitHub
+    let cloneFooterElement = null;
+    $('div.footer__right').find('div').each((i, e) => {
+        $(e).css('display', 'none');
+    })
 
-  let appendElement = $('<div class="footer__static-text"><a class="link link_pale">GitHub</a></div>').click(()=>{ 
-    shell.openExternal('https://github.com/dedpnd/yaradio-yamusic');
-  });
-  $('div.footer__right').append(appendElement);
-  
-  //Add HQ
-  let HQElement =  $('<div class="hq__icon" title="Включить высокое качество"></div>').click(()=>{ 
-    exec('a.toggleHQ()');
-  })
-  $('div.head__right').prepend(HQElement);
+    let appendElement = $('<div class="footer__static-text"><a class="link link_pale">GitHub</a></div>').click(() => {
+        shell.openExternal('https://github.com/dedpnd/yaradio-yamusic');
+    });
+    $('div.footer__right').append(appendElement);
 
-  document.querySelector(".deco-button-overlay").click()
+    //Add HQ
+    let HQElement = $('<div class="hq__icon" title="Включить высокое качество"></div>').click(() => {
+        exec('a.toggleHQ()');
+    })
+    $('div.head__right').prepend(HQElement);
+
+    // document.querySelector(".deco-button-overlay").click()
 }
