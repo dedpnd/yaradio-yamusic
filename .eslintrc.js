e6fe3be8bebca5fbc2eaaff08a0e92450e98c440
dedpnd <pr.deadpanda@gmail.com>
2021-02-24 19:23:38 +0300

    prettier fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "@typescript-eslint/no-explicit-any": false
+        "@typescript-eslint/no-explicit-any": 0
     }
 };
